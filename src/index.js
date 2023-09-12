f4881261de20ec3895702abde7a3cec80e668c32
Muhammad Kamran <typescriptwithkamran@gmail.com>
2023-09-13 02:44:35 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+// to initialize an array to store custmer data
+let custmers = [];
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-        
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+// Data Sturture to store customer information
+
+interface Customer {
+    name: string;
+    debitCardNumber: string;
+    Pin: number;
+    balance: number;
+}
+
+// to initialize an array to store custmer data
+
+let custmers: Customer[] = []; 
